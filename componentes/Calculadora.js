32f74ad28d8fe609e43be233145a0584a93ed8ba
kur0s4k1 <joao12308@hotmail.com>
2019-09-04 17:33:12 -0300

    posicionando as imagens

--- componentes/Calculadora.js
+++ componentes/Calculadora.js
@@ -21,17 +21,161 @@ export default class Calculadora extends Component {
 
     render() {
         return (
-            
+            <ImageBackground source={background} style={{ height: '100%', width: '100%' }} >
+                <View style={{ flex: 1 }}>
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'column'
+                    }}>
+                    </View>
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        alignItems: 'flex-end',
+                        justifyContent: 'center',
+                    }}>
+                        <Text style={styles.texto}>{this.state.texto}</Text>
+                    </View>
+
+                    {/* VIEW DO INPUT */}
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        alignItems: 'stretch',
+                        justifyContent: 'flex-end'
+                    }}>
+                        <TextInput style={styles.textoEntrada} placeholder='INFORME SEU PESO' onChangeText={(peso) => this.setState({ peso: peso })}
+                            keyboardType='numeric' />
+                    </View>
+
+                    {/* COMEMO DAS VIEWS E COLUNAS */}
+                    <View style={{
+                        flex: 2,
+                        flexDirection: 'column'
+                    }}>
+
+                        {/* COMEMO DA 1 COLUNA */}
+                        <View style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            alignItems: 'stretch',
+                            justifyContent: 'space-around'
+                        }}>
+
+                            <TouchableHighlight style={{
+                                flex: 2,
+                                justifyContent: 'space-around',
+                            }}>
+                                <Image source={ac} style={{ width: '100%', height: '100%' }} />
+                            </TouchableHighlight>
+
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around',
+                            }}>
+                                <Image source={lua} style={{ width: '100%', height: '100%' }} />
+                            </TouchableHighlight>
+
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around',
+                            }}>
+                                <Image source={mercurio} style={{ width: '100%', height: '100%' }} />
+                            </TouchableHighlight>
+
+                        </View>
+
+                        {/* COMEMO DA 2 COLUNA */}
+                        <View style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            alignItems: 'stretch',
+                            justifyContent: 'space-between',
+
+                        }}
+                        >
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={venus} style={{ width: '100%', height: '100%' }} />
+                            </TouchableHighlight>
+
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={marte} style={{ width: '100%', height: '100%' }} />
+                            </TouchableHighlight>
+
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={jupiter} style={{ width: '100%', height: '100%' }} />
+                            </TouchableHighlight>
+
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={saturno} style={{ width: '100%', height: '100%' }} />
+                            </TouchableHighlight>
+
+                        </View>
+
+
+                        {/* COMEMO DA 3 COLUNA */}
+                        <View style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            alignItems: 'stretch',
+                            justifyContent: 'space-around',
+
+                        }}>
+                            {/* IMAGEM DO URANO */}
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={urano} style={{ width: '90%', height: '90%' }} />
+                            </TouchableHighlight>
+
+                            {/* IMAGEM DO NETUNO */}
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={netuno} style={{ width: '90%', height: '90%' }} />
+                            </TouchableHighlight>
+
+                            {/*   IMAGEM DO PLUTAO  */}
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={plutao} style={{ width: '90%', height: '90%' }} />
+                            </TouchableHighlight>
+
+
+                            {/* IMAGEM DO SOL */}
+
+                            <TouchableHighlight style={{
+                                flex: 1,
+                                justifyContent: 'space-around'
+                            }}>
+                                <Image source={sol} style={{ width: '80%', height: '80%' }} />
+                            </TouchableHighlight>
+
+                        </View>
+                    </View>
+
+                </View>
+            </ImageBackground>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    texto: {
-        borderColor: 'white',
-        borderWidth: 2,
-        marginTop: 350,
-        fontSize: 15,
-        backgroundColor: 'grey'
-    }
+
 })
